90c3b80d6a9538abec24566debd0d2f4fdd03d34
Joey Haas <josephthaas@gmail.comm>
2021-02-03 13:14:33 -0700

    fixed typo in restaurant class

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant
   attr_reader :opening_time,
-              :name
+              :name          
   def initialize(opening_time, name)
     @opening_time = opening_time
     @name = name
