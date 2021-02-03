15d8bb1cd5c14174d2678d9eb2603f1b685f5b90
Joey Haas <josephthaas@gmail.comm>
2021-02-03 14:02:21 -0700

    added closing_time method to restaurant class

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -5,9 +5,16 @@ class Restaurant
     @opening_time = opening_time
     @name = name
     @dishes = []
+    @closing_time = ()
   end
 
   def dishes
     @dishes
+  end
+
+
+  def closing_time(hours)
+    converted_value = @opening_time.slice(0..1).to_i + hours
+    @closing_time = "#{converted_value}:00"
   end
 end
